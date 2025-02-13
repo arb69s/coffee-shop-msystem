340c072b214f664dc06f4dbf4517a568f8ff2abe
arb69s <arbeerzyba@gmail.com>
2025-02-13 19:20:04 +0100

    SignUp API

---
+++ routes/user.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const connection = require('../connection');
+const router = express.Router();
+
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+var auth = require('../services/authentication');
+var checkRole = require('../services/checkRole');
+
+router.post('/signup', (req, res) => {
+    let user = req.body;
+    query = "select email,password,role,status from user where email=?"
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0) {
+                query = "insert into user(name,contactNumber,email,password,status,role) values(?,?,?,?,'false','user')"
+                connection.query(query, [user.name, user.contactNumber, user.email, user.password], (err, results) => {
+                    if (!err) {
+                        return res.status(200).json({ message: "Successfully registered" });
+                    }
+                    else {
+                        return res.status(500).json(err);
+                    }
+
+                })
+            }
+            else {
+                return res.status(400).json({ message: "Email already exists" });
+            }
+        }
+        else {
+            return res.status(500).json(err);
+        }
+    })
+
+})
+
+
+
+
+
+
+module.exports = router;
