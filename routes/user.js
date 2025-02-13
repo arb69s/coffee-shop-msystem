f5978e9275043998805a63d01ab87c391026b43c
arb69s <arbeerzyba@gmail.com>
2025-02-13 19:26:13 +0100

    Login & Forgot Password API of User
    Login & Forgot Password API of User

--- routes/user.js
+++ routes/user.js
@@ -35,6 +35,74 @@ router.post('/signup', (req, res) => {
     })
 
 })
+router.post('/login', (req, res) => {
+    const user = req.body;
+    query = "select email,password,role,status from user where email=?";
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0 || results[0].password != user.password) {
+                return res.status(401).json({ message: "Incorrect Username or password" });
+            }
+            else if (results[0].status === 'false') {
+                return res.status(401).json({ message: "Wait for admin approval" });
+            }
+            else if (results[0].password == user.password) {
+                const response = { email: results[0].email, role: results[0].role }
+                const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, { expiresIn: '8h' })
+                res.status(200).json({ token: accessToken });
+
+            }
+            else {
+                return res.status(400).json({ message: "Something went wrong.Please try again later" })
+            }
+        }
+        else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pasS: process.env.PASSWORD
+    }
+})
+
+router.post('/forgotPassword', (req, res) => {
+    const user = req.body;
+    query = "select email,password from user where email=?";
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0) {
+                return res.status(200).json({ message: "Password sent successfully to your email. " })
+            }
+            else {
+                var mailOptions = {
+                    from: process.env.EMAIL,
+                    to: results[0].email,
+                    subject: 'Password by Cafe Management System',
+                    html: '<p><b>Your Login details for Coffee Shop managing system</b><br>Email:</b>' + results[0].email + '<br><b>Password:</b>' + results[0].password + '<br><a href="http://localhost:4200/user/login">Click here to login</p>'
+                };
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        console.log(error);
+                    }
+                    else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
+                return res.status(200).json({ message: "Password sent successfully to your email. " })
+
+            }
+        }
+        else {
+            return res.status(500).json(err);
+        }
+
+    })
+})
 
 
 
